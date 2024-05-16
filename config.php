a2d018d6fa036be4be2a2a39eff72d05d9c58f38
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2024-05-16 18:24:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config = [
-    'db_host' => 'localhost',
-    'db_name' => 'your_database',
-    'db_user' => 'your_username',
-    'db_pass' => 'your_password'
+    'db_host' => 'database-1.cf8cyg488n82.ap-south-1.rds.amazonaws.com',
+    'db_name' => 'phpdb',
+    'db_user' => 'admin',
+    'db_pass' => 'admin1234'
 ];
 ?>
